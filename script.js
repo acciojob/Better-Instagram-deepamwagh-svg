dfcf7f72be12bbf3f3857bf13bf3e38a3def29e9
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-01 16:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let draggedDiv = null;
+
+document.querySelectorAll(".image").forEach(div => {
+  div.addEventListener("dragstart", function () {
+    draggedDiv = this;
+    this.classList.add("selected");
+  });
+
+  div.addEventListener("dragover", function (e) {
+    e.preventDefault(); // required to allow drop
+  });
+
+  div.addEventListener("drop", function () {
+    if (draggedDiv === this) return;
+
+    let temp = this.style.backgroundImage;
+    this.style.backgroundImage = draggedDiv.style.backgroundImage;
+    draggedDiv.style.backgroundImage = temp;
+
+    draggedDiv.classList.remove("selected");
+  });
+
+  div.addEventListener("dragend", function () {
+    this.classList.remove("selected");
+  });
+});
